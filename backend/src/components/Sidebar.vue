316ae7bb483d95c33e91f33c54df2ca1cb042f85
Pranab Kalita <pavankalita@gmail.com>
2023-01-31 18:26:34 +0530

    Created Sidebar

---
+++ backend/src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {
+    HomeIcon,
+    UsersIcon,
+    ViewColumnsIcon,
+    ChartBarIcon,
+} from "@heroicons/vue/20/solid";
+</script>
+
+<template>
+    <div class="w-[200px] transition-all bg-indigo-700 text-white py-4 px-2">
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+            <span class="mr-2 text-gray-300">
+                <HomeIcon class="w-5" />
+            </span>
+            <span class="text-sm">Dashboard</span>
+        </router-link>
+
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+            <span class="mr-2 text-gray-300">
+                <ViewColumnsIcon class="w-5" />
+            </span>
+            <span class="text-sm">Products</span>
+        </router-link>
+
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+            <span class="mr-2 text-gray-300">
+                <UsersIcon class="w-5" />
+            </span>
+            <span class="text-sm">Users</span>
+        </router-link>
+
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+            <span class="mr-2 text-gray-300">
+                <ChartBarIcon class="w-5" />
+            </span>
+            <span class="text-sm">Reports</span>
+        </router-link>
+    </div>
+</template>
--- backend/src/layouts/AppLayout.vue
+++ backend/src/layouts/AppLayout.vue
@@ -1,15 +1,22 @@
-<script setup></script>
+<script setup>
+import Sidebar from '../components/Sidebar.vue'
+</script>
 
 <template>
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
-            <div>
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                    {{ title }}
-                </h2>
-            </div>
+    <div class="flex min-h-full">
+        <!-- Sidebar -->
+        <Sidebar />
 
-            <router-view></router-view>
+        <div class="flex-1">
+            <!-- Header -->
+            <header class="h-10 shadow bg-white">
+                Header
+            </header>
+
+            <!-- Content -->
+            <main>
+                <router-view></router-view>
+            </main>
         </div>
     </div>
 </template>
