36b382eacd41251b43eca58a535b862315fc22fe
Pranab Kalita <pavankalita@gmail.com>
2023-01-31 19:10:36 +0530

    Created Not Found Page & Route

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,31 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import AppLayout from "../layouts/AppLayout.vue";
+
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
-import AppLayout from "../layouts/AppLayout.vue";
+import NotFound from "../views/NotFound.vue";
+
+import store from "../store";
 
 const routes = [
     {
         path: "/login",
         name: "login",
+        meta: {
+            requiresGuest: true,
+        },
         component: Login,
     },
     {
         path: "/request-password",
         name: "requestPassword",
+        meta: {
+            requiresGuest: true,
+        },
         component: RequestPassword,
     },
     {
         path: "/reset-password/:token",
         name: "resetPassword",
+        meta: {
+            requiresGuest: true,
+        },
         component: ResetPassword,
     },
     {
         path: "/app",
         name: "app",
         component: AppLayout,
+        meta: {
+            requiresAuth: true,
+        },
         children: [
             {
                 path: "dashboard",
@@ -34,11 +50,26 @@ const routes = [
             },
         ],
     },
+    {
+        path: "/:pathMatch(.*)",
+        name: "notFound",
+        component: NotFound,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "login" });
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({ name: "app.dashboard" });
+    } else {
+        next();
+    }
 });
 
 export default router;
---
+++ backend/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex min-h-full min-w-full w-full justify-center items-center">
+        <div class="flex">
+            <div class="text-indigo-500 text-7xl px-6 font-bold border-r pb-10 self-start">404</div>
+            <div class="px-8">
+                <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
+                <p class="text-gray-600 mb-8">Please check the URL in address bar and try again.</p>
+                <div>
+                    <router-link
+                        :to="{ name: 'app.dashboard' }"
+                        class="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Go
+                        back home</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
