93bf26a9d574e9ed4525c0a5f2ce5d9f0c5b767c
Pranab Kalita <pavankalita@gmail.com>
2023-01-31 15:56:28 +0530

    Installed Vue Router and created login and dashboard routes.

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,26 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    <HelloWorld msg="Vite + Vue" />
+    <router-view></router-view>
 </template>
 
 <style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
