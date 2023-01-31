a3980c66c7b0aaea91adaae62d9c4cfcba19c7bb
Pranab Kalita <pavankalita@gmail.com>
2023-01-31 16:30:21 +0530

    Created RequestPassword and ResetPassword routes and view

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -6,38 +6,33 @@ import { LockClosedIcon } from '@heroicons/vue/20/solid'
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Set New Password
                 </h2>
             </div>
             <form class="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" value="true" />
                 <div class="-space-y-px rounded-md shadow-sm">
                     <div>
-                        <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required=""
+                        <label for="new-password" class="sr-only">New Password</label>
+                        <input id="new-password" type="password" required=""
                             class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Email address" />
+                            placeholder="New password" />
                     </div>
                     <div>
-                        <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password"
-                            required=""
+                        <label for="password-repeat" class="sr-only">Password Repeat</label>
+                        <input id="password-repeat" type="password" required=""
                             class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Password" />
+                            placeholder="Repeat Password" />
                     </div>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox"
-                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-                    </div>
-
                     <div class="text-sm">
-                        <router-link :to="{ name: 'requestPassword' }"
-                            class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your
-                            password?</router-link>
+                        <router-link :to="{ name: 'login' }"
+                            class="font-medium text-indigo-600 hover:text-indigo-500">
+                            Back to Login
+                        </router-link>
                     </div>
                 </div>
 
