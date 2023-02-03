e4fbcf93c8dbd37bbfc4a0c658143e97d42a1b8a
Pranab Kalita <pavankalita@gmail.com>
2023-02-03 20:28:53 +0530

    Created the Pagination in Frontend. Enabled the Search and PerPage in the ProductsController.

--- backend/src/store/products.js
+++ backend/src/store/products.js
@@ -4,24 +4,44 @@ const products = {
     state: () => ({
         loading: false,
         data: [],
+        links: [],
+        from: null,
+        to: null,
+        page: 1,
+        limit: null,
     }),
 
     mutations: {
-        setProducts(state, [loading, data]) {
+        setProducts(state, [loading, data = null]) {
             state.loading = loading;
 
             if (data) {
                 state.data = data.data;
+                state.links = data.meta.links;
+                state.from = data.meta.from;
+                state.to = data.meta.to;
+                state.page = data.meta.current_page;
+                state.limit = data.meta.per_page;
+                state.total = data.meta.total;
             }
         },
     },
 
     actions: {
-        async getProducts({ commit }) {
+        async getProducts(
+            { commit },
+            { url = null, search = "", perPage = 10 }
+        ) {
             commit("setProducts", [true]);
 
             try {
-                const { data } = await axiosClient.get("/products");
+                url = url || "/products";
+                const { data } = await axiosClient.get(url, {
+                    params: {
+                        search,
+                        per_page: perPage,
+                    },
+                });
 
                 commit("setProducts", [false, data]);
             } catch (error) {
--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -16,8 +16,14 @@ onMounted(() => {
 })
 
 // Functions
-const getProducts = async () => {
-    await store.dispatch('getProducts')
+const getProducts = async (url = null) => {
+    await store.dispatch('getProducts', { url, search: search.value, perPage: perPage.value })
+}
+
+const getForPage = async (event, link) => {
+    if (!link.url || link.active) { return }
+
+    await getProducts(link.url)
 }
 </script>
 
@@ -77,6 +83,30 @@ const getProducts = async () => {
                     </tr>
                 </tbody>
             </table>
+
+            <div class="flex justify-between items-center mt-5">
+                <span>
+                    Showing from {{ products.from }} to {{ products.to }}
+                </span>
+
+                <nav v-if="products.total > products.limit"
+                    class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+                    aria-label="Pagination">
+                    <a v-for="(link, i) of products.links" :key="i"
+                        href="#"
+                        aria-current="page"
+                        class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+                        v-html="link.label"
+                        @click.prevent="getForPage($event, link)"
+                        :disabled="!link.url"
+                        :class="[
+                            link.active ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600' : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                            i === 0 ? 'rounded-l-md' : '',
+                            i === products.links.length - 1 ? 'rounded-r-md' : '',
+                            !link.url ? 'bg-gray-100 text-gray-700' : ''
+                        ]"></a>
+                </nav>
+            </div>
         </template>
     </div>
 </template>
