4b19c40ebf270d98641d8a5c015ebccb93a05dc2
Pranab Kalita <pavankalita@gmail.com>
2023-02-08 13:21:20 +0530

    Created ProductModal

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { ref } from 'vue';
+import ProductModal from './ProductModal.vue';
 import ProductsTable from './ProductsTable.vue';
+
+const showModal = ref(false)
+
+const showProductModal = () => {
+    showModal.value = true
+}
 </script>
 
 <template>
@@ -7,9 +15,11 @@ import ProductsTable from './ProductsTable.vue';
         <h1 class="text-3xl font-semibold">Products</h1>
         <button type="submit"
             class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-            @click="showProductModal">Add
-            new Product</button>
+            @click="showProductModal">
+            Add new Product
+        </button>
     </div>
 
+    <ProductModal v-model="showModal" />
     <ProductsTable />
 </template>
